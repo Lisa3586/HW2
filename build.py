dd90a828e845f2b53b2e06c87446196af19e5d91
Lisa <lkhedstrom@gmail.com>
2019-02-12 22:26:24 -0800

    new changes

---
+++ build.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+open("templates/top.html" + "content/index_cont.html" + "templates/bottom.html").write(docs/index-py.html)
+
+open("templates/top.html" + "content/sign-up_cont.html" + "templates/bottom.html").write(docs/sign-up-py.html)
+
+open("templates/top.html" + "content/connect_cont.html" + "templates/bottom.html").write(docs/connect-py.html)
+
+
---
+++ build.sh
@@ -0,0 +1,11 @@
+
+
+
+
+cat templates/top.html content/index_cont.html templates/bottom.html > docs/index.html
+
+cat templates/top.html content/sign-up_cont.html templates/bottom.html > docs/sign-up.html
+
+cat templates/top.html content/connect_cont.html templates/bottom.html > docs/connect.html
+
+
