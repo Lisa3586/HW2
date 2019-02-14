c4cbc2a06090f85dfeec274d0b79a1a416fcbd58
Lisa <lkhedstrom@gmail.com>
2019-02-13 18:29:01 -0800

    finishing up the homework

--- build.py
+++ build.py
@@ -2,12 +2,18 @@
 
 
 
+print ('hello')
 
+top_html = open('templates/top.html').read()
+bottom_html = open('templates/bottom.html').read()
+index_html = open('content/index_cont.html').read()
+sign_up_html = open("content/sign_up_cont.html").read()
+connect_html = open("content/connect_cont.html").read()
 
-open("templates/top.html" + "content/index_cont.html" + "templates/bottom.html").write(docs/index-py.html)
+index_page = top_html + index_html + bottom_html
+sign_up_page = top_html + sign_up_html + bottom_html
+connect_page = top_html + connect_html + bottom_html
 
-open("templates/top.html" + "content/sign-up_cont.html" + "templates/bottom.html").write(docs/sign-up-py.html)
-
-open("templates/top.html" + "content/connect_cont.html" + "templates/bottom.html").write(docs/connect-py.html)
-
-
+open("docs/index-py.html", "w+").write(index_page)
+open("docs/sign-up-py.html", "w+").write(sign_up_page)
+open("docs/connect-py.html", "w+").write(connect_page)
