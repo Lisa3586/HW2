282424e812edd0daedf0302b8fb35c601051b759
Lisa <lkhedstrom@gmail.com>
2019-02-13 18:33:12 -0800

    fix bug in bash script

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 
 cat templates/top.html content/index_cont.html templates/bottom.html > docs/index.html
 
-cat templates/top.html content/sign-up_cont.html templates/bottom.html > docs/sign-up.html
+cat templates/top.html content/sign_up_cont.html templates/bottom.html > docs/sign_up.html
 
 cat templates/top.html content/connect_cont.html templates/bottom.html > docs/connect.html
 
