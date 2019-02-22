99a2bd860bd640e0cc627b132f2c9d47a266e996
Lisa3586 <lkhedstrom@gmail.com>
2019-02-21 18:22:15 -0800

    finishing hw 3
    ^O

--- build.py
+++ build.py
@@ -1,19 +1,46 @@
+pages = [
+         {
+            "filename": "content/index_cont.html",
+            "output": "docs/index.html",
+            "title": "Homer",
+         },
+         {
+            "filename": "content/connect_cont.html",
+            "output": "docs/connect.html",
+            "title": "Connect",
+         },
+         {
+            "filename": "content/sign_up_cont.html",
+            "output": "docs/sign_up.html",
+            "title": "Sign Up",
+         }
+    ]
+    
+#dictionary is common to prevent scoping issues
+
+def main():
+    for page in pages:
+        content =  ( page["filename"])
+        location =  ( page["output"])
+        #place info into variable which will change as we loop. call the writepage function to name each file as it is being looped
+        writepage(content, location)
+    
+def writepage(content, location):
+    #import template files. place in variables
+    top_html = open('templates/top.html').read()
+    bottom_html = open('templates/bottom.html').read()
+    
+    #combine with changing content variable 
+    finished_index_page = top_html + content + bottom_html
+    
+    template = open('templates/base.html').read()
+    content = open(content).read()
+    
+    #replace content with the content being called during the loop
+    finished_index_page = template.replace("{{content}}", content)
+    open(location, 'w+').write(finished_index_page)
 
 
-
-
-print ('hello world')
-
-top_html = open('templates/top.html').read()
-bottom_html = open('templates/bottom.html').read()
-index_html = open('content/index_cont.html').read()
-sign_up_html = open("content/sign_up_cont.html").read()
-connect_html = open("content/connect_cont.html").read()
-
-index_page = top_html + index_html + bottom_html
-sign_up_page = top_html + sign_up_html + bottom_html
-connect_page = top_html + connect_html + bottom_html
-
-open("docs/index-py.html", "w+").write(index_page)
-open("docs/sign-up-py.html", "w+").write(sign_up_page)
-open("docs/connect-py.html", "w+").write(connect_page)
+#call maine function
+if __name__ == "__main__":
+    main()
